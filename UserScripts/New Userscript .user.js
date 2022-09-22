7eb476e697885cf996a0cb20f7c348f95bdcd059
Gaston <naquangaston@gmail.com>
2022-09-22 14:37:13 -0400

    feat: Added New Userscript .user.js programatically

---
+++ UserScripts/New Userscript .user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        // @match        *://greasyfork.org/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
