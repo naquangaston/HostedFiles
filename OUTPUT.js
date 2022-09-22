625fced852eb85cb412d8d4caa031c93e654248c
Gaston <35858201+naquangaston@users.noreply.github.com>
2022-09-22 10:28:21 -0400

    Update OUTPUT.js

--- OUTPUT.js
+++ OUTPUT.js
@@ -1,9 +1,12 @@
-location.host.split('.').map(e=>{
+console.log(location.host)
+var d='https://apps.gwinnett.k12.ga.us/dca/student/dashboard'.split('.').map(e=>{
     return[...(e.match(/\w+/gi)||[])].map(e=>{
+		console.log('--map')
         var l=/[ a-zA-Zw]/gi,n=/[0-9]/gi
         var arr=[]
-        var lc,lcc=1,st='',lcd
-        e.split('').forEach(char=>{
+        var lc=e[0],lcc=1,st='',lcd
+		
+		var cd=char=>{
             lcd=lc
             //console.log(lc,char)
             if(lc){
@@ -18,7 +21,14 @@ location.host.split('.').map(e=>{
             else (lcc++,console.log('none'));
             }
             lc=char
-        })
-        return arr
+        }
+        ;[...e.split(''),''].splice(1).forEach(char=>{
+			//console.log(lc,char)
+			var l=/[a-zA-Z]/gi,n=/\d/gi
+			lc==char?(lcc++):((l.test(lc)||n.test(lc))&&(arr.push(lcc==1?`${lc}`:`${lc}{${lcc}}`)),l.test(lc)&&(arr.push(lcc==1?`${lc}`:`${lc}{${lcc}}`)),console.log(lc,char,lcc,l.test(lc),n.test(lc)),lcc=1);
+			lc=char
+		})
+        return [arr.join(''),e]
     })
 })
+console.log(d)
