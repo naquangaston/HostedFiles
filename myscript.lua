170a4ab32af4b36f99f9e577322bedfbbfc7a2c1
Gaston <35858201+naquangaston@users.noreply.github.com>
2023-08-12 16:07:44 -0400

    Update myscript.lua

--- myscript.lua
+++ myscript.lua
@@ -2049,8 +2049,10 @@ local function hptp()
 				local newPos = enemy.player.Character:WaitForChild("HumanoidRootPart").Position + Vector3.new(randomOffsetX ,randomOffsetY,randomOffsetZ)
 				useAllFire(enemy.player.Character.HumanoidRootPart)
 				if not attacking_ or increaseByPercentage(info.lvl,10) then attacking_ = true else return nil end
+				local cf=game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
 				repeat wait(0);updated_();pcall(function()damageplayer(enemy.player.Name)end);pcall(function()useAllFire(enemy.player.Character.HumanoidRootPart)end); humanoid:MoveTo(enemy.player.Character.HumanoidRootPart.Position) until enemy.player.Character.Humanoid.Health < 1
 								attacking_ = false;
+								game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame=cf
 				--game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame =CFrame.new(newPos)
 
 			else
