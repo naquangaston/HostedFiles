7c323654e33e2d6667818df86d70b4f36848b639
Gaston <35858201+naquangaston@users.noreply.github.com>
2024-05-03 19:24:53 -0400

    Update MooMoo styles.user.js

--- UserScripts/MooMoo styles.user.js
+++ UserScripts/MooMoo styles.user.js
@@ -1,3 +1,22 @@
+// @name MooMoo styles
+// @namespace http://tampermonkey.net/
+// @version 2024-04-23
+// @description try to take over the world!
+// @author You
+// @match *://moomoo.io/*
+// @match *://dev.moomoo.io/*
+// @match *://sploop.io/*
+// @match *://sandbox.moomoo.io/*
+// @match *://tjmoomoo.ml/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=moomoo.io
+// @require https://ksw2-center.glitch.me/users/fzb/antiinvis_1
+// @require http://code.jquery.com/jquery-3.3.1.min.js
+// @require https://code.jquery.com/ui/1.12.0/jquery-ui.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js
+// @require https://raw.githubusercontent.com/naquangaston/HostedFiles/main/UserScripts/Updater.js
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_addValueChangeListener
 function random(e) {
     return e[Math.floor(Math.random() * e.length)]
 }
