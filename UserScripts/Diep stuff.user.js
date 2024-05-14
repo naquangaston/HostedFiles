3ed610458bf444542f30ae96efba0d4d80db388a
Gaston <35858201+naquangaston@users.noreply.github.com>
2024-05-13 23:59:00 -0400

    Add Diep stuff.user.js

---
+++ UserScripts/Diep stuff.user.js
@@ -0,0 +1,21 @@
+// @name Diep stuff
+// @namespace http://tampermonkey.net/
+// @version 2024-05-13
+// @description try to take over the world!
+// @author You
+// @match https://diep.io/
+// @icon https://www.google.com/s2/favicons?sz=64&domain=diep.io
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_info
+// @grant GM_addStyle
+// @grant GM_addValueChangeListener
+// @grant GM_removeValueChangeListener
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @require http://code.createjs.com/easeljs-0.5.0.min.js
+// @require https://cdn.jsdelivr.net/gh/naquangaston/HostedFiles@master/JS_obf.js
+// @require https://cdn.jsdelivr.net/gh/naquangaston/HostedFiles@master/ResourceLoader_.js
+// @require https://cdn.jsdelivr.net/gh/naquangaston/HostedFiles@master/JS_Formatter_.js
+// @require https://raw.githubusercontent.com/naquangaston/HostedFiles/main/UserScripts/Updater.js
+[object Object]
