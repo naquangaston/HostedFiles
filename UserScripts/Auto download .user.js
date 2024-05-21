cf1a0ef7f707d790f5237ba77287fd21c767b024
Gaston <35858201+naquangaston@users.noreply.github.com>
2024-05-21 13:58:19 -0400

    Update Auto download .user.js

--- UserScripts/Auto download .user.js
+++ UserScripts/Auto download .user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Auto download
 // @namespace http://tampermonkey.net/
-// @version 1.5
+// @version 1.7
 // @description try to take over the world!
 // @author You
 // @match *://www.youtube.com/*
