30c53278549a846f805dd42e27f556f79e000666
Gaston <35858201+naquangaston@users.noreply.github.com>
2024-06-13 17:00:50 -0400

    Add ELA Userscript .user.js

---
+++ UserScripts/ELA Userscript .user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name ELA Userscript
+// @namespace http://tampermonkey.net/
+// @version 1.0
+// @description Idk i was vboard and made this
+// @author gaston1779
+// @match *://assessments.macmillanlearning.com/*
+// @match *://achieve.macmillanlearning.com/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=macmillanlearning.com
+// @grant GM_info
+// @grant GM_xmlhttpRequest
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_deleteValue
+// @grant GM_addValueChangeListener
+// @grant GM_removeValueChangeListener
+// @require https://raw.githubusercontent.com/naquangaston/HostedFiles/main/UserScripts/Updater.js
+// @license MIT
+// ==/UserScript==
+const DO_YOU_AGREE_TO_USE_THIS = !1;
+(async function() {
+    return void 0
+})().then(console.log, console.warn);
