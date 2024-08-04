70f14eb3544c300204d298749ba190a3628c7206
Gaston <35858201+naquangaston@users.noreply.github.com>
2024-08-03 22:05:17 -0400

    Add Zoom autoclose min.user.js

---
+++ UserScripts/Zoom autoclose min.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Zoom autoclose
+// @namespace http://tampermonkey.net/
+// @version 0.32
+// @description Autoclose the zoom page when zoom in launched
+// @author You
+// @match *://zoom.us/*
+// @match *://*/*
+// @match https://zoom.us/
+// @grant none
+// @require https://raw.githubusercontent.com/naquangaston/HostedFiles/main/UserScripts/Updater.js
+// @license MIT
+// ==/UserScript==
+document.domain.includes("zoom") && (window.onhashchange = function(o) {
+    window.close()
+}), locaction.hash.includes("suc") && window.close();
