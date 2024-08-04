ecb338938e70d07e446188211c5c69dbe97e1f80
Gaston <35858201+naquangaston@users.noreply.github.com>
2024-08-04 10:27:23 -0400

    Update Zoom autoclose min.user.js

--- UserScripts/Zoom autoclose min.user.js
+++ UserScripts/Zoom autoclose min.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name Zoom autoclose
 // @namespace http://tampermonkey.net/
-// @version 0.32
+// @version 0.33
 // @description Autoclose the zoom page when zoom in launched
 // @author You
-// @match *://zoom.us/*
-// @match *://*/*
-// @match https://zoom.us/
+// @match *://us05web.zoom.us/*
+// @match https://us05web.zoom.us/
 // @grant none
-// @require https://raw.githubusercontent.com/naquangaston/HostedFiles/main/UserScripts/Updater.js
 // @license MIT
+// @downloadURL https://update.greasyfork.org/scripts/502547/Zoom%20autoclose.user.js
+// @updateURL https://update.greasyfork.org/scripts/502547/Zoom%20autoclose.meta.js
+// @require https://raw.githubusercontent.com/naquangaston/HostedFiles/main/UserScripts/Updater.js
 // ==/UserScript==
-document.domain.includes("zoom") && (window.onhashchange = function(o) {
-    window.close()
-}), locaction.hash.includes("suc") && window.close();
+// @license MIT
+console.log("a"), document.domain.includes("zoom") && (window.onhashchange = function(o) {
+    window.close(), history.back()
+}), location.hash.includes("suc") && (window.close(), history.back(), console.log("Back"));
